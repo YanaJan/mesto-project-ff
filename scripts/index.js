ff7906dea750fb984c69475cb2cef38a0f259f5a
Yana Kozlova <yanka.kozlov4@yandex.ru>
2024-01-29 17:48:36 +0300

    2

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const cardTemplate = document.querySelector('#card-template').content;
-const placesList = document.querySelector('.places__list');
+const list = document.querySelector('.places__list');
 
 function createCard(item, removeCard) {
     const card = cardTemplate.cloneNode(true);
@@ -16,5 +16,5 @@ function removeCard(event) {
   }
   
 initialCards.forEach((item) => {
-    placesList.append(createCard(item, removeCard));
+    list.append(createCard(item, removeCard));
   });
